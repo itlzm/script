d438974feaed8a1feaf8c9338912b5c75f6e486a
lzm <15221003637@163.com>
2024-03-15 16:48:49 +0800

    Update egdd.js

--- egdd.js
+++ egdd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/gateway.ergediandian.com\/dduser\/user\/center\/set url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/egdd.js
+^http[s]?:\/\/gateway.ergediandian.com\/dduser\/user\/center\/set url script-response-body https://raw.githubusercontent.com/itlzm/script/main/egdd.js
 
 [mitm] 
 
